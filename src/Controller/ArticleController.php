1aa737511c3ee88a1dd955c9e559a1fa72a8224d
HoussamCzn <10AUUUGH2+HoussamCzn@users.noreply.github.com>
2023-10-26 04:04:59 +0200

    chore: remove unused button

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,7 +9,6 @@ use App\Form\ArticleType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
